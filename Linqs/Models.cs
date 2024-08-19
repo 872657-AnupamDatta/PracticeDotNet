e3fcb4f483d98220fe499ad77902026b66fa45e8
Anupam Datta <anupam.datta2@cognizant.com>
2024-08-19 18:30:46 +0530

    Update Model

--- Linqs/Models.cs
+++ Linqs/Models.cs
@@ -6,5 +6,13 @@
         public string FullName { get; set; }
         public int Age { get; set; }
         public string? State { get; set; }
+        public int StandardID { get; set; }
+    }
+
+    public class Standard
+    {
+        public int ID { get; set; }
+        public string StandardName { get; set; }
+        public int StudentID { get; set; }
     }
 }
